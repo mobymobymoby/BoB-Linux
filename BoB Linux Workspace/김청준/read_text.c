063ddd3952c3ec04f9b45e4b0a85d11c60cb4c08
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-10-24 00:38:33 +0900

    Update read_text.c

--- BoB Linux Workspace/김청준/read_text.c
+++ BoB Linux Workspace/김청준/read_text.c
@@ -13,12 +13,15 @@ int read_txt(char buf[], int n)
   // txt 파일을 읽어온 'buf' array에서 한 글자씩 읽어옴
   // 글자를 읽는 것은 '@'를 만나거나 파일의 끝(NULL)이 될 때 까지
   // 즉 @를 한 문장을 끝내는 플래그로 사용
-	for(; buf[n] != '@' && buf[n] != NULL; n++)
+	for(; buf[n] != '@' && buf[n] != '\0'; n++)
 	{
     
 		printf("%c", buf[n]);
+		// 출력 버퍼 비우기
+		fflush(stdout);
 		// 한 글자씩 출력되는 효과를 위한 sleep
-		usleep(300);
+		usleep(30000);
+		
 	}
 	
 	return n+1;
@@ -34,8 +37,6 @@ void training_mkdir(void)
 	
 	int n = 0;
 	n = read_txt(buf, n);
-	
-	system(" 
 	
 	fclose(fp);
 }
