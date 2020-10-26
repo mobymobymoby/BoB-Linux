42b53f9218a7506bf70de3eb101846b3bc718fec
rltjfkrhgo <48706939+rltjfkrhgo@users.noreply.github.com>
2020-10-26 20:32:24 +0900

    rm

---
+++ BoB Linux Workspace/김기서/training_rm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define BUF_SIZE 1024
+
+void training_mkdir()
+{
+	FILE*  fp = NULL;
+    int    n  = 0;
+	char   buf[BUF_SIZE] = {0,};
+
+    system("touch rm_test_file");
+    system("mkdir rm_test_dir");
+
+	fp = fopen("rm.txt", "r");
+	fread(buf, sizeof(buf), 1, fp);
+
+	n = read_txt(buf, n);
+	run_command("rm rm_test_file");
+
+	n = read_txt(buf, n);
+	run_command("rm -r rm_test_dir");
+    
+    n = read_txt(buf, n);
+	fclose(fp);
+
+	printf("\n");
+}
