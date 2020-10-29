e63bb27f14e67e56c30834eea69248de55069acc
nms200299 <nms200299@naver.com>
2020-10-29 16:13:13 +0900

    Update training_shutdown.c

--- BoB Linux Workspace/노무승/(노무승)리눅스 명령어 트레이닝/training_shutdown.c
+++ BoB Linux Workspace/노무승/(노무승)리눅스 명령어 트레이닝/training_shutdown.c
@@ -81,7 +81,7 @@ int main(){
 
     printf("비슷하게 시스템 종료를 수행하는 명령어로는 'init', 'poweroff', 'halt' 가 있으며,\n");
     sleep(5);
-    printf("시스템 재부팅을 수행하는 명령어로는 'init', 'reboot'이 있습니다.\n");
+    printf("시스템 재부팅을 수행하는 명령어로는 'init', 'reboot'가 있습니다.\n");
     sleep(5);
     printf("\n");
 
