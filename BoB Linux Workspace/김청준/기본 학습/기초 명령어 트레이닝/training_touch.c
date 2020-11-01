159a2d085ab2cdf204afa60d1890ae7a176996f2
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-01 19:10:39 +0900

    Update and rename touch.md to training_touch.c

--- BoB Linux Workspace/김청준/기본 학습/기초 명령어 트레이닝/training_touch.c
+++ BoB Linux Workspace/김청준/기본 학습/기초 명령어 트레이닝/training_touch.c
@@ -1,5 +1,3 @@
-### touch.c
-```
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -12,6 +10,7 @@ void training_touch(void)
 	char def_dir[DIR_SIZE];
 	// rst_dir은 앞에 mkdir, rm -rf의 명령어가 더 들어가기 때문에 문자열의 길이를 10 늘려줌
 	char rst_dir[DIR_SIZE + 10];
+	char rm_dir[DIR_SIZE + 10];
 
 	// 현재 사용자 계정을 %s 위치에 삽입하여 디폴트 디렉토리 설정
 	snprintf(def_dir, sizeof(def_dir), "/home/%s/tr", getlogin());
@@ -20,6 +19,7 @@ void training_touch(void)
 	// rm -rf [디폴트 디렉토리]의 문자열을 rst_dir에 입력
 	snprintf(rst_dir, sizeof(rst_dir), "rm -rf %s", def_dir);
 	system(rst_dir);
+	strncpy(rm_dir, rst_dir, sizeof(rm_dir));
 
 	// 디폴트 디렉토리를 생성
 	// mkdir [디폴트 디렉토리]의 문자열을 rst_dir에 입력
@@ -103,5 +103,8 @@ void training_touch(void)
 	system("ls");
 	
 	printf("touch 명령어에 대한 학습이 끝났습니다.\n");
+	
+	chdir(def_dir);
+	chdir("..");
+	system(rm_dir);
 }
-```
