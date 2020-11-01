2783a60c674ed36acae2277ee84378058e93387b
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-01 19:24:35 +0900

    Update next_quit.c

--- BoB Linux Workspace/김청준/기본 학습/기초 명령어 트레이닝/modules/next_quit.c
+++ BoB Linux Workspace/김청준/기본 학습/기초 명령어 트레이닝/modules/next_quit.c
@@ -4,7 +4,7 @@
 void next_quit()
 {
         char select[CMD_SIZE];
-        printf("\n?ㅼ쓬 紐낅졊?대? ?숈뒿?섏떆?ㅻ㈃ Enter瑜? 醫낅즺?섏떆?ㅻ㈃ 'q'瑜??낅젰?섏꽭??\n");
+        printf("\n다음 명령어를 학습하시려면 Enter를, 종료하시려면 'q'를 입력하세요.\n");
         fgets(select, sizeof(select), stdin);
         select[strlen(select)-1] = '\0';
         if (!strcmp(select, "q"))
