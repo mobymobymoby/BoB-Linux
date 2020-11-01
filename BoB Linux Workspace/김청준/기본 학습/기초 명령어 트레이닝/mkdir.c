7701cf913f440ba2dbb0da0c351509df078d18f3
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-01 18:34:35 +0900

    Update mkdir.c

--- BoB Linux Workspace/김청준/기본 학습/기초 명령어 트레이닝/mkdir.c
+++ BoB Linux Workspace/김청준/기본 학습/기초 명령어 트레이닝/mkdir.c
@@ -12,15 +12,17 @@ void training_mkdir(void)
 	char def_dir[DIR_SIZE];
 	// rst_dir은 앞에 mkdir, rm -rf의 명령어가 더 들어가기 때문에 문자열의 길이를 10 늘려줌
 	char rst_dir[DIR_SIZE + 10];
-
+	char rm_dir[DIR_SIZE + 10];
+	
 	// 현재 사용자 계정을 %s 위치에 삽입하여 디폴트 디렉토리 설정
 	snprintf(def_dir, sizeof(def_dir), "/home/%s/tr", getlogin());
-
+	
 	// 디폴트 디렉토리가 이미 있을 경우를 대비하여 삭제하는 명령어
 	// rm -rf [디폴트 디렉토리]의 문자열을 rst_dir에 입력
 	snprintf(rst_dir, sizeof(rst_dir), "rm -rf %s", def_dir);
 	system(rst_dir);
-
+	strncpy(rst_dir, rm_dir, sizeof(rm_dir));
+	
 	// 디폴트 디렉토리를 생성
 	// mkdir [디폴트 디렉토리]의 문자열을 rst_dir에 입력
 	snprintf(rst_dir, sizeof(rst_dir), "mkdir %s", def_dir);
@@ -52,5 +54,7 @@ void training_mkdir(void)
 	run_command("mkdir -p fruits/apple");
 	
 	printf("mkdir 명령어에 대한 학습이 끝났습니다. \n");
+	
+	system(rm_dir);
 }
 ```
