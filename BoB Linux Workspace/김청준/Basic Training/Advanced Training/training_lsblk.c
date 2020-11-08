8122340433eb2a19c6e33284f6eaaf9600d843a2
black6765 <black6765@gmail.com>
2020-11-09 02:45:17 +0900

    modify getchar -> next_line

--- BoB Linux Workspace/김청준/Basic Training/Advanced Training/training_lsblk.c
+++ BoB Linux Workspace/김청준/Basic Training/Advanced Training/training_lsblk.c
@@ -7,61 +7,61 @@ void training_lsblk(void)
 	//////////////////////////////////////////////////////////////////////////////////////////////////////
 
     printf("이번에 학습할 명령어는 'lsblk'입니다.\n");
-	getchar();
+	next_line();
 	printf("lsblk는 'list block devices'의 약자입니다.\n");
-	getchar();
+	next_line();
 	printf("리눅스에서는 하드디스크, 플로피디스크 등 다양한 디스크 종료의 장치를 'Block'으로 관리합니다.\n");
-	getchar();
+	next_line();
 	printf("이러한 블록 디바이스의 정보를 트리 구조로 볼 수 있는 명령어가 바로 'lsblk'입니다\n");
-	getchar();
+	next_line();
 	printf("아무런 옵션 없이 lsblk를 입력해 현재 연결되어 있는 블록 디바이스의 정보를 확인할 수 있습니다.\n");
-	getchar();
+	next_line();
 	printf("옵션 없이 lsblk를 입력해 디바이스의 정보를 확인해보세요\n사용법은 아래와 같습니다.\n");
 	printf("$ lsblk [옵션]\n");
 	run_command("lsblk");
-	getchar();
+	next_line();
 	printf("첫번째 필드인 'NAME'은 해당 디바이스의 이름을 나타냅니다.\n");
-	getchar();
+	next_line();
 	printf("두번째 필드인 'MAJ:MIN'는 해당 디바이스의 MAJOR 번호와 MINOR 번호를 나타냅니다.\n");
-	getchar();
+	next_line();
 	printf("세번째 필드인 'RM'은 해당 디바이스의 제거 가능 여부(Removable)를 나타냅니다.\n");
 	printf("해당 필드의 값이 0이라면 제거 불가능 상태이며 1이라면 제거 가능한 상태입니다.\n");
-	getchar();
+	next_line();
 	printf("네번째 필드인 'SIZE'는 해당 디바이스의 용량을 나타냅니다.\n");
 	printf("-b 옵션을 추가하면 해당 필드의 값이 바이트(bytes)로 표시됩니다.\n");
-	getchar();
+	next_line();
 	printf("다섯번째 필드인 'RO'은 해당 디바이스의 읽기 전용(Read Only) 여부를 나타냅니다.\n");
 	printf("해당 값이 0이라면 읽기 전용이 아니며, 1이라면 읽기 전용 디바이스 입니다.\n");
-	getchar();
+	next_line();
 	printf("여섯번째 필드인 'TYPE'은 해당 디바이스의 타입을 나타냅니다.\n");
-	getchar();
+	next_line();
 	printf("일곱번째 필드인 'MOUNTPOINT'는 해당 디바이스가 마운트 되어 있는 경로를 표시합니다.\n");
-	getchar();
+	next_line();
 
 	printf("lsblk는 옵션과 함께 사용하여 더욱 다양한 정보를 출력할 수 있습니다.\n");
-	getchar();
+	next_line();
 	printf("-a 옵션은 'all'의 약자로, 비어 있는 디바이스. 즉 용량이 0인 디바이스를 포함하여 출력합니다.\n");
-	getchar();
+	next_line();
 	printf("lsblk와 -a 옵션을 이용해 더 많은 블록 디바이스를 출력해보세요.\n");
 	printf("$ lsblk [옵션]\n");
 	run_command("lsblk -a");
-	getchar();
+	next_line();
 	printf("아까보다 더 많은 디바이스 정보가 출력된 것을 확인할 수 있습니다.\n");
-	getchar();
+	next_line();
 	printf("-p 옵션은 디바이스의 전체 경로를 출력합니다.\n");
-	getchar();
+	next_line();
 	printf("-p 옵션을 이용해 디바이스 목록의 전체 경로를 출력해보세요.\n");
 	printf("$ lsblk [옵션]\n");
 	run_command("lsblk -p");
-	getchar();
+	next_line();
 	printf("NAME 필드에 디바이스 전체 경로가 출력됩니다.\n");
-	getchar();
+	next_line();
 
 	printf("lsblk 명령어는 이처럼 디바이스와 관련한 정보를 볼 수 있기 때문에 mount 명령어와 함께 사용하곤 합니다.\n");
-	getchar();
+	next_line();
 
 	printf("lsblk 명령어 학습이 끝났습니다.\n");
-	getchar();
+	next_line();
 
     // Delete default directory
 	delete_defdir();
--- BoB Linux Workspace/김청준/Basic Training/Essential Training/training_cat.c
+++ BoB Linux Workspace/김청준/Basic Training/Essential Training/training_cat.c
@@ -10,15 +10,15 @@ void training_cat(void)
 	system("echo 'BoB-Linux' > bob.txt");
 
 	printf("이번에 학습할 명령어는 'cat'입니다.\ncat 명령어는 'concatenate'의 약자로, 두 개 이상의 파일을 연결(concatenate)하여 출력할 때 사용합니다.\n");
-	getchar();
+	next_line();
 	printf("간단하게 생각하면, 여러 파일의 내용을 터미널로 연결시켜 출력한다고 보면 됩니다.\n");
-	getchar();
+	next_line();
 	printf("기본 사용법은 다음과 같습니다.\n$ cat [옵션] [파일명]\n");
-	getchar();
+	next_line();
 	printf("다음은 현재 디렉토리에 있는 파일들의 목록입니다.\n");
 	printf("\n");
 	system("ls");
-	getchar();
+	next_line();
 	
 	printf("hello.txt 파일의 내용을 cat 명령어를 이용해 출력해보세요.\n");
 	run_command("cat hello.txt");
@@ -26,22 +26,22 @@ void training_cat(void)
 	printf("hello.txt의 내용인 \"Hello\"가 출력되었습니다.");
 	printf("\n");
 	printf("cat 명령어는 한 번에 두 개의 파일을 연결해서 출력할 수도 있습니다.\n그럴 때에는 간단히 파일명을 띄어쓰기로 구분해서 나열하면 됩니다.\n");
-	getchar();
+	next_line();
 	
 	printf("$ cat [옵션] [파일1] [파일2] [파일3] ...\nhello.txt 파일과 bob.txt 파일을 한 번에 출력해보세요.(hello.txt 다음에 bob.txt를 쓰세요)\n");
 	run_command("cat hello.txt bob.txt");
 	printf("\n");
 	printf("hello.txt의 내용인 \"Hello\"와 bob.txt의 내용인 \"BoB-Linux\"가 출력됩니다.\n");
-	getchar();
+	next_line();
 	
 	printf("cat은 보통 '리다이렉션(redirection)' 이라는 기호 '>'와 함께 자주 쓰입니다.\n지금까지는 간단히 터미널에 파일의 내용을 출력시켰지만, > 기호를 이용하면 특정 파일의 내용을 다른 파일에 연결할 수 있습니다.\n");
-	getchar();
+	next_line();
 	
 	printf("사용법은 다음과 같습니다.\n$ cat [파일 1] [파일 2] ... [파일 n] > [대상 파일명]\n");
-	getchar();
+	next_line();
 	
 	printf("해당 사용법은 [파일 1] [파일 2] ... [파일 n] 까지의 모든 내용을 합쳐, [대상 파일명]로 만듭니다.\n만약 [대상 파일명]이 이미 존재 한다면, 이미 존재하는 파일의 내용을 [파일 1] [파일 2] ... [파일 n]로 덮어 씌웁니다.\n");
-	getchar();
+	next_line();
 	
 	printf("cat 명령어를 이용하여 hello.txt와 bob.txt 파일의 내용을 test.txt로 합쳐보세요.\n");
 	run_command("cat hello.txt bob.txt > test.txt");
@@ -49,40 +49,40 @@ void training_cat(void)
 	printf("아래의 내용은 새로 만들어진 test.txt 파일을 cat 명령어를 통해 출력한 내용입니다.\n");
 	printf("\n");
 	system("cat test.txt");
-	getchar();
+	next_line();
 	
 	printf("두 파일의 내용이 합쳐졌음을 알 수 있습니다. 즉, cat hello.txt bob.txt의 출력 결과가 test.txt 파일에 저장(연결)됩니다.\n");
-	getchar();
+	next_line();
 	
 	printf("cat을 이용해 이미 존재하는 파일에 덮어쓰는게 아닌, 내용을 추가해 덧붙일 수 있습니다.\n");
-	getchar();
+	next_line();
 	
 	printf("사용법은 다음과 같습니다.\n$ cat [파일 1] >> [대상 파일]\n");
-	getchar();
+	next_line();
 
 	printf("만약 [파일 1]의 내용이 \"Linux\"고, [대상 파일]의 내용이 \"BoB\"이라면 명령어를 실행 후 [대상 파일]의 내용은\n");
-	getchar();
+	next_line();
 	
 	printf("\"BoB\"\n\"Linux\"\n가 됩니다. 여기서 주의할 점은 \"BoBLinux\"나 \"BoB Linux\"가 아닌, '개행(new line)'으로 두 파일이 구분된다는 점 입니다.\n");
-	getchar();
+	next_line();
 	
 	printf("리다이렉션에 대해서는 심화 명령어 트레이닝에서 다시 한 번 다루게 됩니다.\n");
-	getchar();
+	next_line();
 	
 	printf("cat의 옵션은 여러 가지가 있습니다. 나열하면 다음과 같습니다.\n");
-	getchar();
+	next_line();
 	
 	printf("-b : 줄번호를 화면 왼쪽에 출력하지만 비어있는 행은 출력하지 않습니다.\n");
 	printf("-n : 줄번호를 화면 왼쪽에 출력하고, 비어있는 행도 출력합니다.\n");
 	printf("-s : 연속되는 2개 이상의 빈 행을 하나의 행으로 출력합니다.\n");
 	printf("-E : 행마다 끝에 $ 문자를 출력합니다.\n");
 	printf("-e : 제어 문자를 ^ 형태로 출력하면서, 각 행의 끝에 $를 출력합니다.\n");
-	getchar();
+	next_line();
 	
 	printf("cat 명령어는 자주 쓰게 될 것이므로, 여러 가지의 텍스트 파일을 이용해 연습하면 좋습니다.\n");
-	getchar();
+	next_line();
 	printf("가장 많이 쓰이는 용법은 cat 명령어를 통해 파일의 내용을 보거나, 리다이렉션 하여 파일의 내용을 변경하는 것 입니다.\n");
-	getchar();
+	next_line();
 	printf("cat 명령어 학습이 끝났습니다.\n");
 
 	delete_defdir();
