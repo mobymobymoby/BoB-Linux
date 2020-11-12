334fa53a6b314fd219b40eedd501306b9dfcba95
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-13 06:30:33 +0900

    Update main.c

--- BoB Linux Workspace/modules/main.c
+++ BoB Linux Workspace/modules/main.c
@@ -9,40 +9,38 @@ int main()
     printf("본 트레이닝 기능은 BoB 9th Team \"Black Moon\"에서 제작하였습니다.\n");
     next_line();
     system("clear");
-    
-    /* 사이트에 대한 설명 */
 
     training_intro();
-
-    printf("본 트레이닝은 '기초 명령어 학습'과 '심화 명령어 학습'으로 나뉩니다.\n");
+    
+    printf("\n본 트레이닝은 '기초 명령어 학습'과 '심화 명령어 학습'으로 나뉩니다.\n");
     next_line();
     printf("기초 명령어 학습은 리눅스를 사용하는데 필요한 내용과 명령어들을 정해진 순서대로 학습합니다.\n");
     printf("심화 명령어 학습은 리눅스를 효율적으로 사용하기 위한 다양한 응용 명령어들을 선택하여 학습할 수 있습니다.\n");
     next_line();
 
-    int menu = 0;
+    char menu;
     while (1)
     {
-        printf("1 : 기초 명령어 학습\n2 : 심화 명령어 학습\n0 : 종료\n실행할 학습을 선택하세요 : ");
-        scanf("%d", &menu);
+        printf("1 : 기초 명령어 학습\n2 : 심화 명령어 학습\nq : 종료\n실행할 학습을 선택하세요 : ");
+        fgets(menu, 1, stdin);
 
-        if (menu == 1)
+        if (menu == '1')
         {
             essential_trainer();
             break;
         }
-        else if (menu == 2)
+        else if (menu == '2')
         {
             advanced_trainer();
             break;
         }
-        else if ( menu == 0)
+        else if ( menu == 'q' || menu == 'Q')
         {
             break;
         }
         else
         {
-            printf("잘못된 번호를 입력했습니다. 다시 입력해주세요.\n");
+            printf("잘못된 입력입니다. 다시 입력해주세요.\n");
         }
     }
     return 0;
