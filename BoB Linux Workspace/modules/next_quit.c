473a21d8cf021ee4339368e1faebffe8dbbc59e0
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-13 09:24:28 +0900

    Update next_quit.c

--- BoB Linux Workspace/modules/next_quit.c
+++ BoB Linux Workspace/modules/next_quit.c
@@ -7,7 +7,7 @@ void next_quit()
         printf("\n다음 명령어를 학습하시려면 Enter를, 종료하시려면 'q'를 입력하세요.\n");
         fgets(select, sizeof(select), stdin);
         select[strlen(select)-1] = '\0';
-        if (!strcmp(select, "q"))
+        if (!strncmp(select, "q", 1) && strlen(select) == 1)
                 exit(0);
         else
                 return;
