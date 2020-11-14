bd62aacbdecc1430cdbf0956a939184cc2ac7b30
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-14 15:37:01 +0900

    Update next_quit.c

--- BoB Linux Workspace/modules/next_quit.c
+++ BoB Linux Workspace/modules/next_quit.c
@@ -8,7 +8,13 @@ void next_quit()
         fgets(select, sizeof(select), stdin);
         select[strlen(select)-1] = '\0';
         if (!strncmp(select, "q", 1) && strlen(select) == 1)
+        {
+                printf("종료를 선택하셨습니다.\n\n");
                 exit(0);
+        }
         else
+        {
+                printf("진행을 선택하셨습니다.\n\n");
                 return;
+        }
 }
