d79cc0752a4808b97e6865c2112f5507b3e8bdef
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-14 15:30:04 +0900

    Update advanced_trainer.c

--- BoB Linux Workspace/modules/advanced_trainer.c
+++ BoB Linux Workspace/modules/advanced_trainer.c
@@ -7,23 +7,26 @@ void advanced_trainer()
     next_line();
     printf("해당 커리큘럼에서는 보안을 위해 리눅스를 사용할 때 응용할 수 있는 명령어를 학습합니다.\n");
     next_line();
-
+    printf("아래의 목록에 따라 원하는 명령어를 선택하여 학습할 수 있습니다.\n");
+    next_line();
+    system("clear");
+    
     char select[20];
     while (1)
-    {
+    {   
         printf("명령어 목록입니다.\n");
         printf("[입력 방법] : [3. head](+tail) -> '3' 또는'head' 입력\n");
-        printf(" [1. file] \t\t [2. find] \n [3. history] \t\t [4. make] \n [5. more](+less) \t [6. mount] \n [7. sort] \t\t [8. tar] \n [9. umask] \t\t [10. uname]\n");
+        printf(" [1. file] \t\t [2. find] \t\t [3. history] \n [4. make] \t\t [5. more](+less) \t [6. mount] \n [7. sort] \t\t [8. tar] \t\t [9. umask] \n [10. uname]\t\t");
         
-        printf(" [11. curl] \t\t [12. group](+관련 명령어) \n [13. head](+tail) \t [14. lsblk] \n [15. mkfs] \t\t [16. ping] \n [17. sed] \t\t [18. user](+관련 명령어+passwd) \n [19. fsck] \t\t [20. halt] \n");
+        printf(" [11. curl] \t\t [12. group](+관련 명령어) \n [13. head](+tail) \t [14. lsblk] \t\t [15. mkfs] \n [16. ping] \t\t [17. sed] \t\t [18. user](+관련 명령어) \n [19. fsck] \t\t [20. halt] \t\t");
 
-        printf(" [21. ifconfig] \t [22. init] \n [23. kill] \t\t [24. nano] \n [25. netstat] \t\t [26. poweroff] \n [27. reboot] \t\t [28. runlevel] \n [29. shutdown] \t [30. su] \n [31. sudo] \t\t [32. top] \n [33. wget] \t\t [34. alias] \n");
+        printf(" [21. ifconfig] \n [22. init] \t\t [23. kill] \t\t [24. nano] \n [25. netstat] \t\t [26. poweroff] \t [27. reboot] \n [28. runlevel] \t [29. shutdown] \t [30. su] \n [31. sudo] \t\t [32. top] \t\t [33. wget] \n [34. alias] \t\t");
         
-        printf(" [35. apt] \t\t [36. chsh] \n [37. cmp] \t\t [38. comm] \n [39. diff] \t\t [40. grep] \n [41. locate] \t\t [42. man] \n [43. pipe] \t\t [44. ps] \n [45. vi] \t\t [46. whereis] \n [47. which] \t\t [48. chmod] \n");
+        printf(" [35. apt] \t\t [36. chsh] \n [37. cmp] \t\t [38. comm] \t\t [39. diff] \n [40. grep] \t\t [41. locate] \t\t [42. man] \n [43. pipe] \t\t [44. ps] \t\t [45. vi] \n [46. whereis] \t\t [47. which] \t\t [48. chmod] \n");
         
-        printf(" [49. chown] \t\t [50. chroot] \n [51. dd] \t\t [52. echo] \n [53. env] \t\t [54. export] \n [55. git] \t\t [56. ln] \n [57. redirection] \t [58. set(+unset)] \n [59. users] \n[q 입력 : 종료]");
+        printf(" [49. chown] \t\t [50. chroot] \t\t [51. dd] \n [52. echo] \t\t [53. env] \t\t [54. export] \n [55. git] \t\t [56. ln] \t\t [57. redirection] \n [58. set(+unset)] \t [59. users] \n [q. 종료]\n");
 
-        printf("원하시는 [ ]안의 명령어의 번호 또는 이름을 입력하세요 : ");
+        printf("\n명령어의 번호 또는 이름을 입력하세요 : ");
 
         int status_code = 1;
         fgets(select, sizeof(select), stdin);
