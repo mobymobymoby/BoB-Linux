09653b2b5574bd7673942a4faa0f6b95bf2ff9a7
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-15 12:29:31 +0900

    Update training_mount.c

--- BoB Linux Workspace/BoB Trainer/all training files/training_mount.c
+++ BoB Linux Workspace/BoB Trainer/all training files/training_mount.c
@@ -10,14 +10,16 @@ void training_mount()
 	printf("리눅스에서는 모든 것이 파일로 다뤄집니다. 디스크 장치 또한 예외가 아닙니다. 이 디스크 장치를 사용하기 위해서 장치 파일과 디렉토리와 연결(mount)해주는 명령어가 mount 명령어 입니다.\n");
 	next_line();
 	printf("우선 mount -l 을 입력하여 현재 마운트 목록을 살펴보세요.\n");
+	
+	// to do : fake_run_command 후에 printf로 예시 제공하여 설명으로 수정
 	run_command("mount -l");
-
+	
 	printf("/dev/sda 와 같이 앞 쪽은 장치를 나타냅니다.\n");
 	printf("on 뒤의 / 는 마운트 된 디렉토리를 나타냅니다.\n");
 	printf("type 뒤에는 파일시스템을 나타냅니다.\n");
 	next_line();
 	printf("mount 명령어는 다음과 같은 형식으로 사용합니다.\n");
-	printf("$ mount [옵션] 마운트할디바이스 연결할디렉토리\n");
+	printf("$ mount [옵션] [마운트할 디바이스] [연결할 디렉토리]\n");
 	next_line();
 	printf("mount 명령어의 옵션은 아래와 같습니다.\n");
 	printf("\t-r : 읽기 전용으로 마운트\n");
