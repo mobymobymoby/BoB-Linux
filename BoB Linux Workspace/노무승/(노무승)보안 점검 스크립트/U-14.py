920f85c1f02bfd82367e190d6ab33e52d8d4a46f
nms200299 <nms200299@naver.com>
2020-11-18 13:53:18 +0900

    Update U-14.py

--- BoB Linux Workspace/노무승/(노무승)보안 점검 스크립트/U-14.py
+++ BoB Linux Workspace/노무승/(노무승)보안 점검 스크립트/U-14.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def check(user):
+def U14(user):
     flag = False
     if (user == "root"):
         path = "/etc/"
@@ -20,23 +20,30 @@ def check(user):
             path = "/home/" + i + "/"
         for j in list:
             output = subprocess.getoutput("ls -aldGQ " + path + j)
-            if (output.find("No such file or directory")==-1): # if setting file exist
+            if (output.find("No such file or directory")==-1) and (output.find("그런 파일이나 디렉터리가 없습니다")==-1): # if setting file exist
                 if (output[5] == 'w'): # GID write able check
-                    print("[경고] " + path + j + " : 그룹에 소속된 사용자(GID)가 해당 파일을 쓸 수 있습니다.")
+                    print("     [경고] " + path + j + " : 그룹에 소속된 사용자(GID)가 해당 파일을 쓸 수 있습니다.")
                     flag = True
                 if (output[8] == 'w'): # Other User write able check
-                    print("[경고] " + path + j + " : 다른 모든 사용자가 해당 파일을 쓸 수 있습니다.")
+                    print("     [경고] " + path + j + " : 다른 모든 사용자가 해당 파일을 쓸 수 있습니다.")
                     flag = True
                 output = output.split("\"")
                 if (output[0].find("root")==-1) : # File Owner check
                     if (output[0].find(i)==-1) :
-                        print("[경고] " + path + j + " : 파일의 소유권(UID)이 '" + i + "'계정이 아닙니다.")
+                        print("     [경고] " + path + j + " : 파일의 소유권(UID)이 '" + i + "'계정이 아닙니다.")
                         flag = True
  
     if (flag==False):
-        print("[검사 결과] 안전합니다.\n")
+        print("     [검사 결과] 안전합니다.\n")
+        return False
     else:
-        print("[검사 결과] 보안이 필요합니다.\n")
+        print("     [검사 결과] 보안이 필요합니다.\n")
+        return True
 
-check("root")
-check("user")
+def U14_report():
+    print("조치 방법")
+    print("     'chmod og-w [파일명]'으로 소유자외 다른 유저의 파일 실행을 막을 수 있습니다.")
+    print("     'chown [소유자] [파일명]'으로 소유자를 변경할 수 있습니다.")
+
+if (U14("root")) or (U14("user")) :
+    U14_report()
