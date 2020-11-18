9247abee680223be40282567a670e183d326c0a3
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-18 11:30:35 +0900

    Update training_wget.c

--- BoB Linux Workspace/BoB Trainer/all training files/training_wget.c
+++ BoB Linux Workspace/BoB Trainer/all training files/training_wget.c
@@ -8,9 +8,10 @@ void file_check(){
 void training_wget(void)
 {
     create_defdir();
-	system("clear");
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
+    system("clear");
+	
 
+    printf("이번에 배울 명령어는 'wget'입니다.\n\n");
     printf("wget 명령어는 웹 상에 있는 파일을 다운받는 명령어이며,\n");
     printf("지원되는 프로토콜은 HTTP, HTTPS, FTP가 있습니다.\n");
     next_line();
@@ -23,9 +24,21 @@ void training_wget(void)
     file_check();
     run_command("wget https://www.naver.com");
     next_line();
-    // 트레이닝 시작
-
-    printf("[기타 유용한 옵션들]\n");
+    printf("wget을 통해 다운로드 받은 파일에 대한 정보가 출력됩니다.\n\n");
+    printf("접속한 URL의 IP 정보와 '200'이라는 HTTP 상태코드 등\n여러가지 정보를 확인할 수 있습니다.\n");
+    printf("또한, 우리가 방금 다운로드 받은 파일은 용량이 낮은 파일이라 확인하기 힘들었겠지만,\n파일이 다운로드 받아지는 상태가 표시됩니다.\n");
+	
+    printf("\n다운로드 받은 파일인 'index.html'을 확인해보겠습니다.\n");
+    printf("[실습] cat을 이용하여 index.html의 내용을 확인해보세요.\n");
+    just_run_command("cat index.html");
+	
+    printf("우리가 네이버에 접속하여 볼 수 있는 내용이 텍스트로 출력됩니다.\n\n");
+    printf("\n이렇게 html 파일을 다운로드 받을 수도 있지만,\n우리가 흔히 인터넷에서 다운로드 받는 자료들도 wget으로 받을 수 있습니다.\n");
+    printf("인터넷에서 클릭을 통해 다운로드를 받는 방식은\n어떤 아이콘을 클릭하면 그에 해당하는 링크로 이동되는 원리입니다.\n\n");
+    printf("따라서 해당 링크 주소를 얻을 수 있다면, \n동일한 파일을 wget [해당 주소] 명령어를 통해 다운로드 받을 수 있습니다.\n");
+    printf("wget 명령어는 'curl' 명령어와 흡사한 점이 많으니 \n비교하며 공부해보시길 권해드립니다.\n");
+	
+    printf("[기타 유용한 옵션들]\n\n");
     printf("wget -c [URL] : 파일 이어받기\n");
     printf("wget -0 [URL] : 파라미터 형태의 링크에서 파일 다운로드\n");
     printf("wget -r [URL] : 디렉터리 구조를 유지한채 다운로드\n");
@@ -34,7 +47,11 @@ void training_wget(void)
     next_line();
     printf("이와 같이 옵션을 사용하면 더욱 유용하게 활용할 수 있습니다.\n");
     next_line();
+    printf("이후에 콘솔 혹은 터미널에 \"man\" 명령어나 \"[명령어] --help\"와 같이 '--help' 옵션을 통해\n");
+    printf("더 많은 정보와 옵션들을 확인하시길 바랍니다.\n");
+    next_line();
+    printf("\"wget\" 명령어의 학습이 끝났습니다.\n");
+    printf("고생하셨습니다.\n");
 
     delete_defdir();
-    printf("고생하셨습니다!\n");
 }
