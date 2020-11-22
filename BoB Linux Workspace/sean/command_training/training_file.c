70660578fad54b20483e23735e63e9c19024dc9f
songseon <songseon0110@gmail.com>
2020-11-22 23:20:57 +0900

    update training_file.c

--- BoB Linux Workspace/sean/command_training/training_file.c
+++ BoB Linux Workspace/sean/command_training/training_file.c
@@ -27,26 +27,26 @@ void training_file()
     printf("텍스트 파일임을 알 수 있습니다.\n");
 	next_line();
 
-	printf("[디렉토리 관련 지식]");
+	printf("[디렉토리 관련 지식]\n");
 	printf("리눅스에서는 디렉토리도 파일로 취급합니다.\n");
-	printf(".  은 현재 디렉토리를 가리키는 파일입니다.\n");
-	printf(".. 은 상위 디렉토리를 가리키는 파일입니다.\n");
+	printf("'.'  은 현재 디렉토리를 가리키는 파일입니다.\n");
+	printf("'..' 은 상위 디렉토리를 가리키는 파일입니다.\n\n");
 	next_line();
 
 	printf("[실습] \"file .\"(을)를 입력하여 현재 디렉토리 경로를 의미하는\n");
     printf("' . '이 어떤 종류인지 확인해보세요.\n");
 	just_run_command("file .");
 
-	printf("\n위의 결과를 보시면 \"file\" 명령어로 디렉토리인지도 확인이 가능합니다.\n");
+	printf("\n위의 결과를 보시면 \"file\" 명령어로 디렉토리인지도 확인이 가능합니다.\n\n");
 	next_line();
 
-    printf("\"file\" 명령어의 자주 쓰이는 옵션은 다음과 같습니다.\n\n");
-    printf("' -b (--brief)  : 지정한 파일명은 출력하지 않고 파일의 유형만 출력합니다.\n");
-    printf("' -f (--file-from [파일 목록]) : [파일 목록]에서 지정한 파일들에 대해서만 명령을 실행합니다.\n");
-    printf("' -i (--mime)   : MIME 타입 문자로 출력합니다.\n");
-    printf("' -L (--dereference)    : 심볼릭 링크된 파일을 추적하여 원본 파일 정보를 출력합니다.\n");
-    printf("' -m (--magic-file [매직 파일] : 임의로 [매직 파일]을 지정합니다.\n");
-    printf("' -z (--umcompress)     : 압축된 파일의 내용을 출력해줍니다.\n");
+    printf("\"file\" 명령어의 자주 쓰이는 옵션은 다음과 같습니다.\n\n\n");
+    printf("[-b (--brief)]\n지정한 파일명은 출력하지 않고 파일의 유형만 출력합니다.\n\n");
+    printf("[-f (--file-from [파일 목록])]\n[파일 목록]에서 지정한 파일들에 대해서만 명령을 실행합니다.\n\n");
+    printf("[-i (--mime)]\nMIME 타입 문자로 출력합니다.\n\n");
+    printf("[-L (--dereference)]\n심볼릭 링크된 파일을 추적하여 원본 파일 정보를 출력합니다.\n\n");
+    printf("[-m (--magic-file [매직 파일])]\n임의로 [매직 파일]을 지정합니다.\n\n");
+    printf("[-z (--umcompress)]\n압축된 파일의 내용을 출력해줍니다.\n\n\n");
     next_line();
 
     printf("\"file\" 명령어의 옵션 '-b'와 '-i'를 실습해보겠습니다.\n");
@@ -56,7 +56,7 @@ void training_file()
     printf("사용법 : file [옵션] [파일 이름]\n");
     just_run_command("file -b test.txt");
 
-    printf("\n아까 실습했던 \"file test.txt\"(와)과는 다르게 파일의 이름은 출력해주지 않고\n");
+    printf("\n처음에 실습했던 \"file test.txt\"(와)과는 다르게 파일의 이름은 출력해주지 않고\n");
     printf("파일의 종류만 출력해줍니다.\n\n");
     next_line();
 
@@ -64,7 +64,7 @@ void training_file()
     printf("사용법 : file [옵션] [파일 이름]\n");
     just_run_command("file -i test.txt");
 
-    printf("\n위의 결과는 지금까지 실습했던 결과와는 보여주는 것이 좀 다를 것입니다.\n");
+    printf("\n위의 결과는 지금까지 실습했던 결과와는 보여주는 것이 좀 다를 것입니다.\n\n");
     printf("\"text/plain\" 부분은 \"파일의 종류/파일의 포맷\" 형태입니다.\n");
     printf("그리고 \"charset=us-ascii\"는 '문자가 us-ascii로 작성되어있다'라는 뜻입니다.\n");
     next_line();
