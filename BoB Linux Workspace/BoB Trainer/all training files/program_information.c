89ed904567c17fef416c74c22b8c4cd6c85414ba
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-24 08:51:38 +0900

    Update program_information.c

--- BoB Linux Workspace/BoB Trainer/all training files/program_information.c
+++ BoB Linux Workspace/BoB Trainer/all training files/program_information.c
@@ -3,15 +3,15 @@
 void program_information(void)
 {
 	system("clear");
-	printf("Thanks for using our trainer\n");
-	printf("This Trainer mady by BoB 9th Team 'Black Moon'\n");
+	printf("Thanks for using our trainer.\n");
+	printf("This Trainer made by BoB 9th Team 'Black Moon'\n");
 	printf("\n");
-	printf("Our goal is to educate [security beginners]\n");
+	printf("Our goal is to educate [security beginners].\n");
 
-	printf("Our project repository is open\n");
+	printf("Our project repository is open.\n");
 	printf("Github repository : https://github.com/black6765/BoB-Linux\n");
 
-	printf("\nIf you have any question, please contact below information\n");
+	printf("\nIf you have any question, please contact below information.\n");
 
 	printf("\nblack6765@gmail.com(KIM CHEONG-JUN, Project Manager)\n");
 }
