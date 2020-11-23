278d4e87d90d8f4662099f94d96eaa292ac4e316
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-24 08:43:07 +0900

    Create select_menu.c

---
+++ BoB Linux Workspace/BoB Trainer/all training files/select_menu.c
@@ -0,0 +1,48 @@
+void select_menu(void)
+{
+    char menu[20] = "";
+    printf("본 트레이닝은 크게 '기초 명령어 학습'과 '심화 명령어 학습'으로 나뉩니다.\n\n");
+    printf("기초 명령어 학습은 리눅스를 사용하는데 필요한 내용과 명령어들을 정해진 순서대로 학습합니다.\n\n");
+    printf("심화 명령어 학습은 리눅스를 효율적으로 사용하기 위한 다양한 응용 명령어들을 선택하여 학습할 수 있습니다.\n");
+    next_line();
+    while (1)
+    {
+        printf("1 : 리눅스 기초\n2 : 기초 명령어 학습\n3 : 심화 명령어 학습\n0 : 프로그램 정보\nq : 종료\n실행할 학습을 선택하세요 : ");
+
+        fgets(menu, sizeof(menu), stdin);
+
+        // 나머지 입력 값 제거
+        menu[strlen(menu) - 1] = '\0';
+
+        // 입력값 검증 부분
+        if (!strncmp(menu, "1", strlen("1")) && strlen(menu) == strlen("1"))
+        {
+            training_linux_basic();
+            break;
+        }
+        else if (!strncmp(menu, "2", strlen("2")) && strlen(menu) == strlen("2"))
+        {
+            essential_trainer();
+            break;
+        }    
+        else if (!strncmp(menu, "3", strlen("3")) && strlen(menu) == strlen("3"))
+        {
+            advanced_trainer();
+            break;
+        }
+        else if (!strncmp(menu, "0", strlen("0")) && strlen(menu) == strlen("0"))
+        {
+            program_information();
+            break;
+        }
+        else if ( (!strncmp(menu, "q", strlen("q")) && strlen(menu) == strlen("q")) || (!strncmp(menu, "Q", strlen("Q")) && strlen(menu) == strlen("Q")) )
+        {
+            break;
+        }
+        else
+        {
+            system("clear");
+            printf("잘못된 입력입니다. 다시 입력해주세요.\n\n");
+        }
+    }
+}
