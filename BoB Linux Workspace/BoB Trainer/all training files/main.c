da62d93bf8473d46fe0538466110365be1790fc4
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-24 08:43:01 +0900

    Update main.c

--- BoB Linux Workspace/BoB Trainer/all training files/main.c
+++ BoB Linux Workspace/BoB Trainer/all training files/main.c
@@ -14,48 +14,9 @@ int main()
 
     training_intro();
 
-    printf("본 트레이닝은 '기초 명령어 학습'과 '심화 명령어 학습'으로 나뉩니다.\n\n");
-    printf("기초 명령어 학습은 리눅스를 사용하는데 필요한 내용과 명령어들을 정해진 순서대로 학습합니다.\n\n");
-    printf("심화 명령어 학습은 리눅스를 효율적으로 사용하기 위한 다양한 응용 명령어들을 선택하여 학습할 수 있습니다.\n");
-    next_line();
-
-    char menu;
-    while (1)
-    {
-        printf("1 : 리눅스 기초\n2 : 기초 명령어 학습\n3 : 심화 명령어 학습\n0 : 프로그램 정보\nq : 종료\n실행할 학습을 선택하세요 : ");
-        menu = getchar();
-        __fpurge(stdin);
-        
-        
-        if (menu == '1')
-        {
-            training_linux_basic();
-            break;
-        }
-        else if (menu == '2')
-        {
-            essential_trainer();
-            break;
-        }
-        else if (menu == '3')
-        {
-            advanced_trainer();
-            break;
-        }
-        else if (menu == '0')
-        {
-            program_information();
-            break;
-        }
-        else if ( menu == 'q' || menu == 'Q')
-        {
-            break;
-        }
-        else
-        {
-            system("clear");
-            printf("잘못된 입력입니다. 다시 입력해주세요.\n\n");
-        }
-    }
+    select_menu(void);
+    
+    printf("\n[트레이너를 종료합니다.]\n");
+    
     return 0;
 }
