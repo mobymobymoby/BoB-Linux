f9dbd52b09870e04b53b3aa9aeee9b4d1e2c1116
songseon <songseon0110@gmail.com>
2020-11-25 04:04:26 +0900

    update chsh.c env.c history.c locate.c

--- Workspace/sean/command_training/training_locate.c
+++ Workspace/sean/command_training/training_locate.c
@@ -13,7 +13,8 @@ void training_locate(void)
     next_line();
     
     printf("\"locate\" 명령어의 실행 파일은 \"usr/bin/locate\"(으)로 있습니다.\n");
-    printf("'root' 디렉토리(폴더) 밑에 'usr' 디렉토리(폴더) 밑에 'bin' 디렉토리(폴더) 밑에 \"locate\"(으)로 있습니다.\n");
+    printf("'root' 디렉토리(폴더) 밑에 'usr' 디렉토리(폴더) 밑에 'bin' 디렉토리(폴더) 밑에\n");
+    printf("\"locate\"(으)로 있습니다.\n");
     next_line();
     
     printf("\"find\" 명령어는 다양한 옵션이 있어서 원하는 파일을 거의 완벽하게 찾을 수 있지만\n");
@@ -26,7 +27,8 @@ void training_locate(void)
     printf("검색하고 싶은 문자열이나 패턴만으로 검색할 때는 locate가 더 빠르고 좋습니다.\n");
     next_line();
     
-    printf("하지만 \"locate\" 명령어는 속도는 빠르지만 사용 전에 \"sudo updatedb\"라는 명령어로 DB 파일을 생성 혹은 업데이트를 해줘야 한다는 번거로움이 있습니다.\n");
+    printf("하지만 \"locate\" 명령어는 속도는 빠르지만 사용 전에 \"sudo updatedb\"라는 명령어로\n");
+    printf("DB 파일을 생성 혹은 업데이트를 해줘야 한다는 번거로움이 있습니다.\n");
     next_line();
     
     printf("그래도 매번 사용 전에 해주면 좋겠지만 최근에 무언가를 삭제하는 등의\n");
@@ -34,7 +36,8 @@ void training_locate(void)
     printf("시스템에서 자동으로 매일 새벽에 자동으로 업데이트 작업을 진행해줍니다.\n");
     next_line();
     
-    printf("만약 시스템에서 \"locate\" 명령어가 실행되지 않는다면 \"mlocate\"라는 패키지를 설치해주어야 합니다.\n");
+    printf("만약 시스템에서 \"locate\" 명령어가 실행되지 않는다면\n");
+    printf("\"mlocate\"라는 패키지를 설치해주어야 합니다.\n");
     printf("Debian 계열의 시스템에서는 \"sudo apt install mlocate\"를 입력하여 설치해줍니다.\n");
     next_line();
     
@@ -43,16 +46,17 @@ void training_locate(void)
     next_line();
     
     printf("\"locate\" 명령어를 사용 전에 \"sudo updatedb\" 명령어로 DB를 업데이트 해줘야 합니다.\n");
-    printf("기본으로 Ubuntu에서는 \"/var/lib/mlocate/mlocate.db\"로 DB가 존재하고 지정되어있습니다.\n");
+    printf("기본으로 Ubuntu에서는 \"/var/lib/mlocate/mlocate.db\"로 DB가 존재하고 지정되어있습니다.\n\n");
     next_line();
     
     printf("[실습] \"sudo updatedb\"라고 입력하여 DB를 업데이트 해주세요.\n");
     just_run_command("sudo updatedb");
     
-    printf("\nDB가 성공적으로 업데이트 되었을 것입니다.\n");
+    printf("\n\n이제 DB가 성공적으로 업데이트 되었을 것입니다.\n\n");
     next_line();
     
-    printf("[실습] \"locate\" 명령어를 이용하여 \"find\"라는 단어가 들어간 파일들의 경로들을 출력하세요.\n");
+    printf("[실습] \"locate\" 명령어를 이용하여\n");
+    printf("\"find\"라는 단어가 들어간 파일들의 경로들을 출력하세요.\n");
     printf("사용법 : locate [검색할 명령어]\n");
     just_run_command("locate find");
     
@@ -64,15 +68,15 @@ void training_locate(void)
     next_line();
     
     printf("-e : 검색에서 제외할 디렉토리를 지정합니다.\n");
-    printf("-n : 결과에서 몇 개만 보여줄 것인지 제한합니다.\n");
+    printf("-l : 결과에서 몇 개만 보여줄 것인지 제한합니다.\n");
     next_line();
     
-    printf("실행 결과에서 n개만 보여주겠끔 하는 옵션을 실습하겠습니다.\n");
+    printf("실행 결과에서 n개만 보여주겠끔 하는 옵션을 실습하겠습니다.\n\n");
     next_line();
     
-    printf("[실습] \"locate -n 10 *.deb\"을(를) 입력하여 -n 옵션을 실습해보세요");
+    printf("[실습] \"locate -l 10 *.deb\"을(를) 입력하여 -n 옵션을 실습해보세요\n");
     printf("사용법 : locate [옵션] [검색할 문자열 or 패턴]\n");
-    just_run_command("locate -n 10 *.deb");
+    just_run_command("locate -l 10 *.deb");
     
     printf("\n잘 입력하셨다면, .deb 확장자를 가진 파일들의 경로가 출력되었을 것입니다.\n");
     printf("(아무것도 출력되지 않는다면 '.deb' 확장자의 파일이 없다는 것입니다.)\n");
