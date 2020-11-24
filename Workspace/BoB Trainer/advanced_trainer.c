1e84ae7965bd3749fb59bee5746b3099a30b2b13
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-24 13:05:10 +0900

    Update advanced_trainer.c

--- Workspace/BoB Trainer/advanced_trainer.c
+++ Workspace/BoB Trainer/advanced_trainer.c
@@ -13,16 +13,16 @@ void advanced_trainer()
     {   
         system("clear");
         printf("명령어 목록입니다.\n");
-        printf("입력 방법 : [3. head](+tail) -> '3' 또는'head' 입력\n\n");
-        printf(" [1. file] \t\t [2. find] \t\t [3. history] \n [4. make] \t\t [5. more](+less) \t [6. mount] \n [7. sort] \t\t [8. tar] \t\t [9. umask] \n [10. uname]\t\t");
+        printf("입력 방법 : [22. head](+tail) -> '22' 또는 'head' 입력\n\n");
+        printf(" [1. alias] \t\t [2. apt] \t\t [3. chmod] \n [4. chown] \t\t [5. chroot] \t [6. chsh] \n [7. cmp] \t\t [8. comm] \t\t [9. curl] \n [10. dd]\t\t");
         
-        printf(" [11. curl] \t\t [12. group](+관련 명령어) \n [13. head](+tail) \t [14. lsblk] \t\t [15. mkfs] \n [16. ping] \t\t [17. sed] \t\t [18. user](+관련 명령어) \n [19. fsck] \t\t [20. halt] \t\t");
+        printf(" [11. diff] \t\t [12. echo] \n [13. env] \t [14. export] \t\t [15. file] \n [16. find] \t\t [17. fsck] \t\t [18. git] \n [19. grep] \t\t [20. group](+관련 명령어) \t\t");
 
-        printf(" [21. ifconfig] \n [22. init] \t\t [23. kill] \t\t [24. nano] \n [25. netstat] \t\t [26. poweroff] \t [27. reboot] \n [28. runlevel] \t [29. shutdown] \t [30. su] \n [31. sudo] \t\t [32. top] \t\t [33. wget] \n [34. alias] \t\t");
+        printf(" [21. halt] \n [22. head](+tail) \t\t [23. history] \t\t [24. ifconfig] \n [25. init] \t\t [26. kill] \t [27. ln] \n [28. locate] \t [29. lsblk] \t [30. make] \n [31. man] \t\t [32. mkfs] \t\t [33. more](+less) \n [34. mount] \t\t");
         
-        printf(" [35. apt] \t\t [36. chsh] \n [37. cmp] \t\t [38. comm] \t\t [39. diff] \n [40. grep] \t\t [41. locate] \t\t [42. man] \n [43. pipe] \t\t [44. ps] \t\t [45. vi] \n [46. whereis] \t\t [47. which] \t\t [48. chmod] \n");
+        printf(" [35. nano] \t\t [36. netstat] \n [37. ping] \t\t [38. pipe] \t\t [39. poweroff] \n [40. ps] \t\t [41. reboot] \t\t [42. redirection] \n [43. runlevel] \t\t [44. sed] \t\t [45. set](+unset) \n [46. shutdown] \t\t [47. sort] \t\t [48. su] \n");
         
-        printf(" [49. chown] \t\t [50. chroot] \t\t [51. dd] \n [52. echo] \t\t [53. env] \t\t [54. export] \n [55. git] \t\t [56. ln] \t\t [57. redirection] \n [58. set(+unset)] \t [59. users] \n [q. 종료]\n");
+        printf(" [49. sudo] \t\t [50. tar] \t\t [51. top] \n [52. umask] \t\t [53. uname] \t\t [54. user](+관련 명령어) \n [55. users] \t\t [56. vi] \t\t [57. wget] \n [58. whereis] \t [59. which] \n [q. 종료]\n");
 
         printf("\n명령어의 번호 또는 이름을 입력하세요 : ");
 
