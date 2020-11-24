19d74fc5e515da66f1a560c9825f2093a9c8a357
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-24 11:36:18 +0900

    Update training_sort.c

--- BoB Linux Workspace/BoB Trainer/all training files/training_sort.c
+++ BoB Linux Workspace/BoB Trainer/all training files/training_sort.c
@@ -13,11 +13,12 @@ void training_sort()
 	printf("하지만, 옵션 없이 사용시 오름차 순으로 정렬된 결과를 출력만 해줄 뿐, \n따로 저장하거나 원본 파일을 변경하지는 않습니다.\n");
 	next_line();
 	printf("sort 명령어는 다음과 같은 형식으로 사용합니다.\n");
-	printf("$ sort [옵션] 파일\n");
+	printf("$ sort [옵션] [파일 이름]\n");
 	next_line();
 	printf("[실습] 우선 cat으로 정렬 전의 'testfile' 파일의 내용을 확인해봅시다.\n");
 	just_run_command("cat testfile");
 	printf("\n정렬되지 않은 상태의 파일 내용이 출력됩니다.\n");
+	next_line();
 	printf("[실습] 이번엔 sort를 이용하여 'testfile' 파일을 내용을 정렬시켜 확인해보세요.\n");
 	just_run_command("sort testfile");
 	printf("\n아까와는 달리 오름차순(a-b-c.. 순)으로 정렬됩니다.\n");
@@ -30,7 +31,7 @@ void training_sort()
 	printf("\t-r : 내림차순으로 정렬\n");
 	printf("\t-o : 정렬된 내용을 지정된 파일명에 저장\n\n");
 	
-	printf("-o 옵션을 이용해 정렬된 내용을 특정 파일에 저장할 수 있습니다.\n");
+	printf("sort와 -o 옵션을 이용해 정렬된 내용을 특정 파일에 저장할 수 있습니다.\n");
 	next_line();
 	printf("사용법은 다음과 같습니다.\n");
 	printf("$ sort -o [원본 파일] [대상 파일]\n");
@@ -45,7 +46,7 @@ void training_sort()
 	next_line();
 	printf("[실습] cat으로 정렬 후의 'testfile' 파일의 내용을 확인해봅시다.\n");
 	just_run_command("cat testfile");
-	printf("파일의 내용이 정렬되어 저장된 것을 확인할 수 있습니다.\n");
+	printf("\n파일의 내용이 오름차순으로 정렬되어 저장된 것을 확인할 수 있습니다.\n");
 	next_line();
 	printf("sort 명령어는 파이프라인(|) 혹은 리다이렉션(>) 기호와 같이 자주 사용됩니다.\n");
 	next_line();
@@ -59,7 +60,7 @@ void training_sort()
 	printf("\"sort\" 명령어의 학습이 끝났습니다.\n");
 	printf("고생하셨습니다.\n");
 
-	system("rm sort_test_file");
+	system("rm testfile");
 
 	delete_defdir();
 }
