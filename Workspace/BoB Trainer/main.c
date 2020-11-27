136fac4657cedd96088a83324e487cabb5360bd7
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-27 23:43:38 +0900

    Update main.c

--- Workspace/BoB Trainer/main.c
+++ Workspace/BoB Trainer/main.c
@@ -7,9 +7,12 @@ int main()
 
     printf("BoB Linux Training에 오신 것을 환영합니다.\n");
     printf("(Enter 키를 눌러 진행할 수 있습니다)\n\n");
+    
     printf("본 트레이닝 기능은 BoB 9th Team \"Black Moon\"에서 제작하였습니다.\n\n");
+    
     printf("이어서 트레이닝에 앞서 기본적인 사항을 설명드리겠습니다.\n");
     next_line();
+    
     system("clear");
 
     training_intro();
@@ -17,6 +20,8 @@ int main()
     select_menu();
     
     printf("\n[트레이너를 종료합니다]\n\n");
+    printf("[Enter]를 입력하세요.\n");
+    next_line();
     
     return 0;
 }
