07b86f71f51d50d8d1fba9a3b090cc08e8267fc5
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-27 23:45:19 +0900

    Update print_banner.c

--- Workspace/BoB Trainer/print_banner.c
+++ Workspace/BoB Trainer/print_banner.c
@@ -25,4 +25,14 @@ void print_banner()
     usleep(100000);
     printf("                                                     |____/ \n\n");
     usleep(100000);
+    
+    printf("BoB Linux Training에 오신 것을 환영합니다.\n");
+    printf("(Enter 키를 눌러 진행할 수 있습니다)\n\n");
+    
+    printf("본 트레이닝 기능은 BoB 9th Team \"Black Moon\"에서 제작하였습니다.\n\n");
+    
+    printf("이어서 트레이닝에 앞서 기본적인 사항을 설명드리겠습니다.\n");
+    next_line();
+    
+    system("clear");
  }
