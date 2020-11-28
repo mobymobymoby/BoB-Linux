765f9b45696d2829dd289c14c0c9593bbb26ff5b
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-11-28 20:56:24 +0900

    Update training_linux_basic.c

--- Workspace/BoB Trainer/training_linux_basic.c
+++ Workspace/BoB Trainer/training_linux_basic.c
@@ -7,13 +7,10 @@ void training_linux_basic(void)
     
     char menu[20] = "";
     printf("환영합니다.\n\n");
-    printf("여기서는 리눅스가 무엇인지와 종류, 필요성 그리고 기본 디렉토리들을 배울 것입니다.\n\n\n");
-    printf("1. 리눅스란 무엇인가\n\n");
-    printf("2. 리눅스 배포판과 종류\n\n");
-    printf("3. 리눅스의 필요성\n\n");
-    printf("4. 리눅스의 기본 디렉토리들\n");
-    next_line();
+    printf("리눅스 기본 지식 트레이닝입니다.\n");
+    printf("리눅스가 무엇인지와 종류, 필요성 그리고 기본 디렉토리들을 학습할 수 있습니다.\n\n\n");
     
+    next_line();
     while (1)
     {
         printf("1 : 리눅스란 무엇인가\n2 : 리눅스 배포판과 종류\n3 : 리눅스의 필요성\n4 : 리눅스의 기본 디렉토리들\nq : 종료(quit)\n\n실행할 학습을 선택하세요 : ");
@@ -54,7 +51,6 @@ void training_linux_basic(void)
             printf("잘못된 입력입니다. 다시 입력해주세요.\n\n");
         }
     }
-           
     system("clear");
     
     /*
@@ -63,6 +59,7 @@ void training_linux_basic(void)
     printf("이 트레이닝에서 배운 것만으로는 많이 부족할 것입니다.\n");
     printf("추후에 꼭 구글에 검색을 해보시거나 서적 등을 통해 더 많이 공부하시길 바랍니다.\n\n");
     */
+    printf("리눅스 기본 지식 트레이닝을 종료합니다.\n");
     printf("고생하셨습니다.\n");
     
     delete_defdir();
