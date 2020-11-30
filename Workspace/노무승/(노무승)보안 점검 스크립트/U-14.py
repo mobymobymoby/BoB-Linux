ab2b499e2828b958a17a428bf5db0c3369f96fef
nms200299 <nms200299@naver.com>
2020-12-01 04:49:07 +0900

    Update U-14.py

--- Workspace/노무승/(노무승)보안 점검 스크립트/U-14.py
+++ Workspace/노무승/(노무승)보안 점검 스크립트/U-14.py
@@ -7,52 +7,63 @@
 import subprocess
 import os.path
 
-def U14(user):
-    flag = False
-    if (user == "root"):
-        path = "/etc/"
-        user = ["root"]
-        list = "environment bash.bashrc profile bashrc profile.d"
-        list = list.split()
-        print("[U-14] 공통 환경 변수를 검사합니다.")
-    elif (user == "user"):   
-        user = subprocess.getoutput("ls /home")
-        user = user.split() # User Count
-        list = ".bashrc .bash_profile .bash_login .bash_logout .profile .kshrc .cshrc .login .exrc .netrc"
-        list = list.split()
-        print("[U-14] 사용자별 환경 변수를 검사합니다.")
+C_END     = "\033[0m"
+C_RED    = "\033[31m"
+C_GREEN  = "\033[32m"
+C_YELLOW = "\033[33m"
 
-    for i in user:
-        if (i != "root"):
-            path = "/home/" + i + "/"
-        for j in list:
-            if os.path.exists(path + j) :  # if setting file exist
-                output = subprocess.getoutput("ls -aldGQ " + path + j)
-                if (output[5] == 'w'): # GID write able check
-                    print("\t[경고] " + path + j + " : 그룹에 소속된 사용자(GID)가 해당 파일을 쓸 수 있습니다.")
-                    flag = True
-                if (output[8] == 'w'): # Other User write able check
-                    print("\t[경고] " + path + j + " : 다른 모든 사용자가 해당 파일을 쓸 수 있습니다.")
-                    flag = True
-                output = output.split("\"")
-                if (output[0].find("root")==-1) : # File Owner check
-                    if (output[0].find(i)==-1) :
-                        print("\t[경고] " + path + j + " : 파일의 소유권(UID)이 '" + i + "'계정이 아닙니다.")
+def U14():
+    output = ""
+    f_output = ""
+    chk_user = ["root","user"]
+    for user in chk_user:
+        flag = False
+        if (user == "root"):
+            path = "/etc/"
+            user = ["root"]
+            list = "environment bash.bashrc profile bashrc profile.d"
+            list = list.split()
+            f_output = f_output + "[U-14] 공통 환경 변수 검사\n"
+        elif (user == "user"):   
+            user = subprocess.getoutput("ls /home")
+            user = user.split() # User Count
+            list = ".bashrc .bash_profile .bash_login .bash_logout .profile .kshrc .cshrc .login .exrc .netrc"
+            list = list.split()
+            f_output = f_output + "[U-14] 사용자별 환경 변수 검사\n"
+
+        for i in user:
+            if (i != "root"):
+                path = "/home/" + i + "/"
+            for j in list:
+                if os.path.exists(path + j) :  # if setting file exist
+                    output = subprocess.getoutput("ls -aldGQ " + path + j)
+                    if (output[5] == 'w'): # GID write able check
+                        f_output = f_output + C_YELLOW + "\t[경고] " + path + j + " : 그룹에 소속된 사용자(GID)가 해당 파일을 쓸 수 있습니다.\n" + C_END
                         flag = True
- 
-    if (flag==False):
-        print("\t[검사 결과] 안전합니다.\n")
-    else:
-        print("\t[검사 결과] 보안이 필요합니다.\n")
-    return flag
+                    if (output[8] == 'w'): # Other User write able check
+                        f_output = f_output + C_YELLOW + "\t[경고] " + path + j + " : 다른 모든 사용자가 해당 파일을 쓸 수 있습니다.\n" + C_END
+                        flag = True
+                    output = output.split("\"")
+                    if (output[0].find("root")==-1) : # File Owner check
+                        if (output[0].find(i)==-1) :
+                            f_output = f_output + C_YELLOW + "\t[경고] " + path + j + " : 파일의 소유권(UID)이 '" + i + "'계정이 아닙니다.\n" + C_END
+                            flag = True
+    
+        if (flag) :
+            f_output = f_output + C_RED + "\t[검사 결과] 보안 조치가 필요합니다.\n" + C_END  
+            f_output = f_output + "\n"
+            if (f_output.find("조치 방법") == -1):
+                f_output = f_output + "[U-14] 조치 방법\n"
+                f_output = f_output + "\t1. 'chmod og-w [파일명]'으로 소유자외 다른 유저의 파일 실행을 막을 수 있습니다.\n"
+                f_output = f_output + "\t2. 'chown [소유자] [파일명]'으로 소유자를 변경할 수 있습니다.\n"
+                f_output = f_output + "\n"
+        else :
+            f_output = f_output + C_GREEN + "\t[검사 결과] 안전합니다.\n" + C_END
+            f_output = f_output + "\n"
 
-def U14_report():
-    print("[U-14] 조치 방법")
-    print("\t1. 'chmod og-w [파일명]'으로 소유자외 다른 유저의 파일 실행을 막을 수 있습니다.")
-    print("\t2. 'chown [소유자] [파일명]'으로 소유자를 변경할 수 있습니다.")
-    print("")
+    print(f_output,end='')
+    handle = open("./U-14.txt", mode='w', encoding='utf-8')
+    handle.write(f_output)
+    handle.close()
 
-U14flag = False
-if (U14("root")): U14flag = True
-if (U14("user")): U14flag = True
-if (U14flag) : U14_report()
+U14()
