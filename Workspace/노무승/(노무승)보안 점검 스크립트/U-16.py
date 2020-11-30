74e1834bad2bde2fdc1c087b9e302b883ae827bd
nms200299 <nms200299@naver.com>
2020-12-01 04:50:28 +0900

    Update U-16.py

--- Workspace/노무승/(노무승)보안 점검 스크립트/U-16.py
+++ Workspace/노무승/(노무승)보안 점검 스크립트/U-16.py
@@ -5,25 +5,33 @@
 
 import subprocess
 
+C_END     = "\033[0m"
+C_RED    = "\033[31m"
+C_GREEN  = "\033[32m"
+C_YELLOW = "\033[33m"
+
 def U16():
-    print("[U-16] /dev 디렉터리에서 불필요한 파일을 검사합니다.")
-    output = subprocess.getoutput("find /dev -type f -exec ls -al {} \; 2>/dev/null")
+    f_output = ""
+    f_output = f_output + "[U-16] /dev 디렉터리 불필요한 파일 검사\n"
+
+    output = subprocess.getoutput("find /dev -type f 2>/dev/null")
     output = output.split("\n")
 
     if (output == ['']) : # Nothing
-        print("\t[검사 결과] 안전합니다.\n")
-        return False
+        f_output = f_output + C_GREEN + "\t[검사 결과] 안전합니다.\n" + C_END
+        f_output = f_output + "\n"
     else : # Detect
         for i in output :
-            if (i[0]=="-") :
-                print("\t" + i)
-        print("\t[검사 결과] 보안이 필요합니다.\n")
-        return True
-        
-def U16_report():
-    print("[U-16] 조치 방법")
-    print("\t1. 중요한 파일이면 'mv [파일명] [디렉터리]'로 파일을 옮기십시오.")
-    print("\t2. 'rm -rf [파일명]'으로 /dev 디렉터리에 존재하는 쓸데없는 파일을 삭제하십시오.")
-    print("")
+            f_output = f_output + C_YELLOW + "\t[경고] " + i + "\n" + C_END
+        f_output = f_output + C_RED + "\t[검사 결과] 보안 조치가 필요합니다.\n" + C_END  
+        f_output = f_output + "\n"
+        f_output = f_output + "[U-16] 조치 방법\n"
+        f_output = f_output + "\t1. 중요한 파일이면 'mv [파일명] [디렉터리]'로 파일을 옮기십시오.\n"
+        f_output = f_output + "\t2. 'rm -rf [파일명]'으로 /dev 디렉터리에 존재하는 쓸데없는 파일을 삭제하십시오.\n"
 
-if(U16()) : U16_report()
+    print(f_output,end='')
+    handle = open("./U-16.txt", mode='w', encoding='utf-8')
+    handle.write(f_output)
+    handle.close()
+
+U16()
