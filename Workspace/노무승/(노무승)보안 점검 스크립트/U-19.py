04b5f0a19265bae64956213d530cfd23c54b938b
nms200299 <nms200299@naver.com>
2020-12-01 04:51:16 +0900

    Update U-19.py

--- Workspace/노무승/(노무승)보안 점검 스크립트/U-19.py
+++ Workspace/노무승/(노무승)보안 점검 스크립트/U-19.py
@@ -5,15 +5,21 @@
 
 import subprocess
 
+C_END = "\033[0m"
+C_RED= "\033[31m"
+C_GREEN  = "\033[32m"
+C_YELLOW = "\033[33m"
+
 def U19():
-    print("[U-19] finger 서비스 설치 여부를 검사합니다.")
+    f_output = ""
+    f_output = f_output + "[U-19] finger 서비스 설치 여부 검사\n"
     flag = False
 
     # if install dpkg (exam. Debian Base (Ubuntu/Mint))
     output = subprocess.getoutput("dpkg --get-selections | grep finger")
     output = output.split()
     if (output == ['finger','install']) :
-        print("\t[경고] finger 서비스가 설치되어 있습니다.") 
+        f_output = f_output + C_YELLOW + "\t[경고] finger 서비스가 설치되어 있습니다.\n" + C_END
         flag = True
 
     # if not install dpkg (exam. Redhat Base)
@@ -21,21 +27,25 @@ def U19():
         output = subprocess.getoutput("finger")
         output = output.split()
         if (output[0]=="Login") and (output[1]=="Name") and (output[2]=="Tty") :
-            print("\t[경고] finger 서비스가 설치되어 있습니다.") 
+            f_output = f_output + C_YELLOW + "\t[경고] finger 서비스가 설치되어 있습니다.n" + C_END
             flag = True
     
     if (flag) :
-        print("\t[검사 결과] 보안이 필요합니다.\n")
+        f_output = f_output + C_RED + "\t[검사 결과] 보안 조치가 필요합니다.\n" + C_END  
+        f_output = f_output + "\n"
+        f_output = f_output + "[U-19] 조치 방법\n"
+        f_output = f_output + "\t1. Debian 계열 (Ubuntu, Mint 등)의 경우,\n"
+        f_output = f_output + "\t\t'apt remove finger -y'로 패키지를 삭제하십시오.\n"
+        f_output = f_output + "\t2. Redhat 계열 (Redhat, CentOS 등)의 경우,\n"
+        f_output = f_output + "\t\t'yum remove finger -y'로 패키지를 삭제하십시오.\n"
     else :
-        print("\t[알림] finger 서비스가 설치되어 있지 않습니다.")
-        print("\t[검사 결과] 안전합니다.\n")
-    return flag
+        f_output = f_output + "\t[알림] finger 서비스가 설치되어 있지 않습니다.\n"
+        f_output = f_output + C_GREEN + "\t[검사 결과] 안전합니다.\n" + C_END
+        f_output = f_output + "\n"
 
-def U19_report():
-    print("[U-19] 조치 방법")
-    print("\t1. Debian 계열 (Ubuntu, Mint 등)의 경우,")
-    print("\t\t'apt remove finger -y'로 패키지를 삭제하십시오.")
-    print("\t2. Redhat 계열 (Redhat, CentOS 등)의 경우,")
-    print("\t\t'yum remove finger -y'로 패키지를 삭제하십시오.")
+    print(f_output,end='')
+    handle = open("./U-19.txt", mode='w', encoding='utf-8')
+    handle.write(f_output)
+    handle.close()
 
-if(U19()) : U19_report()
+U19()
