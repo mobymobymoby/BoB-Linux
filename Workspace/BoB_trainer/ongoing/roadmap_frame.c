6bc181e0422c675b74ccfd27f0997e7af8f8b924
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-01 19:26:25 +0900

    Upload frame files

---
+++ Workspace/BoB_trainer/ongoing/roadmap_frame.c
@@ -0,0 +1,6 @@
+#include "func.h"
+
+void roadmap_company(void)
+{
+	printf("보안 관련 기업 파트입니다.\n");
+}
---
+++ Workspace/BoB_trainer/ongoing/roadmap_job_dev.c
@@ -0,0 +1,6 @@
+#include "func.h"
+
+void roadmap_job_dev(void)
+{
+	printf("보안 관련 기업 파트입니다.\n");
+}
