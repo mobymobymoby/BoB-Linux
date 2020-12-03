7ba5e53d6c9dbad738c679bedbc37020a1566b92
sean-baek <songseon0110@gmail.com>
2020-12-03 20:36:39 +0900

    update func.h

--- Workspace/BoB_trainer/next_quit_roadmap.c
+++ Workspace/BoB_trainer/next_quit_roadmap.c
@@ -1,6 +1,6 @@
 #include "func.h"
 
-void next_quit_roadmap()
+void next_quit_roadmap(void)
 {
         char select[CMD_SIZE];
         printf("\n다음 로드맵 트레이닝을 진행하시려면 [Enter]를, 종료하시려면 [q]를 입력하세요.\n");
