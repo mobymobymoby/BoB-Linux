50e059d325a35e5594050711d67d8f076512f16b
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-03 11:13:48 +0900

    Update roadmap_job_consult.c.c

--- Workspace/BoB_trainer/ongoing/roadmap_job_consult.c.c
+++ Workspace/BoB_trainer/ongoing/roadmap_job_consult.c.c
@@ -2,5 +2,110 @@
 
 void roadmap_job_consult(void)
 {
-	printf("보안 관련 기업 파트입니다.\n");
+	system("clear");
+	printf("[보안 로드맵]\n");
+	printf("└ [보안 직업군]\n");
+	printf("  └ [보안 컨설턴트]\n\n");
+
+	printf("이번에 살펴볼 보안 직업군은 \"보안 컨설턴트\" 입니다.\n\n");
+	printf("\"보안 컨설턴트\"는 기업에서 보안 컨설팅을 담당하는 직업군입니다.\n");
+	next_line();
+	
+	printf("일정 규모 이상의 기업들은 자산을 보호하기 위해서\n");
+	printf("보안에 대한 여러 가지 측면을 고려하게 됩니다.\n");
+	next_line();
+
+	printf("따라서 전문적인 보안 업무 능력을 갖춘 \"보안 컨설턴트\"를 고용하여\n");
+	printf("기업의 전반적인 보안을 점검할 수 있도록 맡기는 것입니다.\n");
+	next_line();
+	
+	// 보안 컨설턴트의 업무
+	printf("그렇다면 \"보안 컨설턴트\"는 어떤 업무를 수행할까요?\n");
+	next_line();
+	
+	printf("기본적으로 기업에서는 법으로 정한 보안 점검 사항을 지킬 의무가 있습니다.\n");
+	printf("하지만 이러한 점검 사항을 기업 내부에서 자체적으로 점검하기엔 난이도가 있습니다.\n");
+	next_line();
+	
+	printf("\"보안 컨설턴트\"는 이러한 법적 점검 사항을 체크하고\n");
+	printf("그 결과를 기업 내의 담당자에게 알기 쉽게 설명해줍니다.\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf
 }
