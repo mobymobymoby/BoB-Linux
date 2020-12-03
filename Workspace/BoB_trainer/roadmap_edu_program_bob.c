0015f13f124acb73214f5bc5226277556494a08a
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-03 17:48:14 +0900

    Create roadmap_edu_program_bob.c

---
+++ Workspace/BoB_trainer/roadmap_edu_program_bob.c
@@ -0,0 +1,180 @@
+#include "func.h"
+
+void roadmap_edu_program_bob(void)
+{
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+    
+    printf("\n");
+    printf("\n");
+    next_line();
+}
