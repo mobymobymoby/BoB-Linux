2fdf7ae884a7620d1187cc0087170c41355352f6
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-03 14:14:26 +0900

    Update roadmap_job.c

--- Workspace/BoB_trainer/ongoing/roadmap_job.c
+++ Workspace/BoB_trainer/ongoing/roadmap_job.c
@@ -16,7 +16,7 @@ void roadmap_job(void)
         system("clear");
         printf("[보안 로드맵]\n");
         printf("  └ [보안 직업군]\n\n");
-        printf("1 : 보안 관제\n2 : 모의 해킹 전문가\n3 : 보안 컨설턴트\n4 : 보안 제품 개발자\n5 : 디지털 포렌식\n6 : 악성코드 분석가\nq : 종료(quit)\n\n실행할 직업군을 선택하세요 : ");
+        printf("1 : 보안 관제\n2 : 모의 해킹 전문가\n3 : 보안 컨설턴트\n4 : 보안 제품 개발자\n5 : 디지털 포렌식 전문가\n6 : 악성코드 분석가\nq : 종료(quit)\n\n실행할 직업군을 선택하세요 : ");
 
         fgets(menu, sizeof(menu), stdin);
 
