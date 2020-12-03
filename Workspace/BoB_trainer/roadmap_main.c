2c4316d4bbaf09ca871de542dbff0dfa93d346e0
sean-baek <songseon0110@gmail.com>
2020-12-03 20:52:33 +0900

    update roadmap_main.c

--- Workspace/BoB_trainer/roadmap_main.c
+++ Workspace/BoB_trainer/roadmap_main.c
@@ -12,7 +12,14 @@ void roadmap_main(void)
     {
         system("clear");
         printf("[보안 로드맵]\n\n");
-        printf("1 : 보안 직업\n2 : 보안 세부 분야\n3 : 보안 관련 학과\n4 : 보안 관련 기업\n5 : 보안 / 해킹 대회\n6 : 보안 관련 교육 프로그램\nq : 종료(quit)\n\n실행할 로드맵을 선택하세요 : ");
+        printf("1 : 보안 직업\n");
+	printf("2 : 보안 세부 분야\n");
+	printf("3 : 보안 관련 학과\n");
+	printf("4 : 보안 관련 기업\n");
+	printf("5 : 보안 / 해킹 대회\n");
+	printf("6 : 보안 관련 교육 프로그램\n");
+	printf("q : 종료(quit)\n\n");
+	printf("실행할 로드맵을 선택하세요 : ");
 
         fgets(menu, sizeof(menu), stdin);
 
