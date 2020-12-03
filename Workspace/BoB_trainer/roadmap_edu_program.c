b88ea5186b831d2c3ec56bf682b0d7c1024f580b
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-03 21:48:25 +0900

    Update roadmap_edu_program.c

--- Workspace/BoB_trainer/roadmap_edu_program.c
+++ Workspace/BoB_trainer/roadmap_edu_program.c
@@ -17,7 +17,7 @@ void roadmap_edu_program(void)
         system("clear");
         printf("[보안 로드맵]\n");
    	printf("  └ [보안 관련 교육 프로그램]\n\n");
-        printf("1 : BoB(Best of the Best)\n2 : K-Shield\nq : 종료(quit)\n\n실행할 교육 프로그램을 선택하세요 : ");
+        printf("1 : BoB(Best of the Best)\n2 : K-Shield Jr.\nq : 종료(quit)\n\n실행할 교육 프로그램을 선택하세요 : ");
 
         fgets(menu, sizeof(menu), stdin);
 
