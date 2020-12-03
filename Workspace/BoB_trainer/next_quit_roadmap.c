9af28e7a39cedb0be089622dd070668fba7d7c64
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-03 20:03:11 +0900

    Update and rename next_quit.c to next_quit_roadmap.c

--- Workspace/BoB_trainer/next_quit_roadmap.c
+++ Workspace/BoB_trainer/next_quit_roadmap.c
@@ -1,10 +1,9 @@
 #include "func.h"
 
-
-void next_quit()
+void next_quit_roadmap()
 {
         char select[CMD_SIZE];
-        printf("\n다음 트레이닝을 진행하시려면 [Enter]를, 종료하시려면 [q]를 입력하세요.\n");
+        printf("\n다음 로드맵 트레이닝을 진행하시려면 [Enter]를, 종료하시려면 [q]를 입력하세요.\n");
         fgets(select, sizeof(select), stdin);
         select[strlen(select)-1] = '\0';
         if (!strncmp(select, "q", 1) && strlen(select) == 1)
