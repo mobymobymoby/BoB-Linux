0067283eca69e8c855f38d26874217a6ba3e5e41
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-04 09:54:44 +0900

    Create company files

---
+++ Workspace/BoB_trainer/roadmap_company_wins.c
@@ -0,0 +1,117 @@
+#include "func.h"
+
+void roadmap_company_wins(void)
+{
+	system("clear");
+	printf("[보안 로드맵]\n");
+	printf("  └ [보안 관련 기업]\n");
+	printf("      └ [BoB - Best of the Best]\n\n");
+
+	printf("이번에 살펴볼 보안 관련 기업은 \"안랩(Ahnlab)\"입니다.\n\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+	
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+
+
+	printf("\"\" 보안 관련 기업 로드맵이 종료되었습니다.\n");
+}
