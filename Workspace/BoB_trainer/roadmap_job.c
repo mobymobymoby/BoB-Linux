22bcf708b40f68ffbd158c399e360fac6ab4d6cc
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-04 20:48:52 +0900

    Update roadmap_job.c

--- Workspace/BoB_trainer/roadmap_job.c
+++ Workspace/BoB_trainer/roadmap_job.c
@@ -7,7 +7,7 @@ void roadmap_job(void)
     printf("  └ [보안 직업군]\n\n");
 
     printf("보안 직업군 파트입니다.\n\n");
-    printf("해당 파트에서는 보안과 관련된 일을 하는 직업들을 소개합니다\n");
+    printf("해당 파트에서는 보안과 관련된 직업들을 소개합니다\n");
     next_line();
 
     char menu[20] = "";
