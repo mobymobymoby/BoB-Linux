d48154b2fdd2955421a55c84beb01bdf4ecbc95f
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-04 16:13:00 +0900

    Update and rename roadmap_company_igloo.c to roadmap_company_fsec.c

---
+++ Workspace/BoB_trainer/roadmap_company_fsec.c
@@ -0,0 +1,94 @@
+#include "func.h"
+
+void roadmap_company_fsec(void)
+{
+	system("clear");
+	printf("[보안 로드맵]\n");
+	printf("  └ [보안 관련 기업]\n");
+	printf("      └ [금융보안원]\n\n");
+
+	// 인트로
+	printf("이번에 살펴볼 보안 관련 기관은 \"금융보안원\"입니다.\n\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+	printf("\n");
+	printf("\n");
+	next_line();
+
+
+
+	printf("\"금융보안원\" 보안 관련 기관 로드맵이 종료되었습니다.\n");
+}
