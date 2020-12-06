a05a0d7799620c111abdc92523fc23b0eb0542ea
rltjfkrhgo <48706939+rltjfkrhgo@users.noreply.github.com>
2020-12-07 08:47:14 +0900

    색상 출력 수정

--- Workspace/김기서/보안점검/printModule.py
+++ Workspace/김기서/보안점검/printModule.py
@@ -11,17 +11,21 @@ def printTitle(report, string):
     report.write(string + '\n')
 
 def printSafe(report):
-    print('\t[검사결과] ' + C_GREEN + '안전' + C_END + '합니다.\n')
+    print('\t' + C_GREEN + '[검사결과] 안전합니다.' + C_END + '\n')
     report.write('\t[검사결과] 안전합니다.\n\n')
 
 def printNotsafe(report):
-    print('\t[검사결과] ' + C_RED + '보안 조치' + C_END + '가 필요합니다.\n')
+    print('\t' + C_RED + '[검사결과] 보안 조치가 필요합니다.' + C_END + '\n')
     report.write('\t[검사결과] 보안 조치가 필요합니다.\n\n')
 
 def printWarning(report, string):
-    print('\t[' + C_YELLOW + '경고' + C_END + '] ' + string)
+    print('\t' + C_YELLOW + '[경고] ' + string + C_END)
     report.write('\t[경고] ' + string + '\n')
 
 def printNotice(report, string):
     print('\t[알림] ' + string)
     report.write('\t[알림] ' + string + '\n')
+
+def printSolution(report, string):
+    print(string)
+    report.write(string)
