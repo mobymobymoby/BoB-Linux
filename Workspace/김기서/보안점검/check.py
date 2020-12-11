0f3aecaa1ca7fe6dfae6b68912f51900d8efb134
nms200299 <nms200299@naver.com>
2020-12-11 15:51:01 +0900

    Update check.py

--- Workspace/김기서/보안점검/check.py
+++ Workspace/김기서/보안점검/check.py
@@ -1,6 +1,6 @@
 import os
 
-checkList = {'01': 'root 계정 원격 접속 제한', \
+checkList_1 = {'01': 'root 계정 원격 접속 제한', \
              '02': '패스워드 복잡성 설정', \
              '03': '계정 잠금 임계값 설정', \
              '04': '패스워드 파일 보호', \
@@ -14,7 +14,8 @@ checkList = {'01': 'root 계정 원격 접속 제한', \
              '51': '계정이 존재하지 않는 GID 금지', \
              '52': '동일한 UID 금지', \
              '53': '사용자 shell 점검', \
-             '54': 'Session Timeout 설정', \
+             '54': 'Session Timeout 설정'}
+checkList_2 = {
              '05': 'root 홈, 패스 디렉터리 권한 및 패스 설정', \
              '06': '파일 및 디렉터리 소유자 설정', \
              '07': '/etc/passwd 파일 소유자 및 권한 설정', \
@@ -34,7 +35,8 @@ checkList = {'01': 'root 계정 원격 접속 제한', \
              '57': 'UMASK 설정 관리', \
              '58': '홈디렉토리 소유자 및 권한 설정', \
              '59': '홈디렉토리로 지정한 디렉토리의 존재 관리', \
-             '60': '숨겨진 파일 및 디렉토리 검색 및 제거', \
+             '60': '숨겨진 파일 및 디렉토리 검색 및 제거'}
+checkList_3 = {
              '19': 'finger 서비스 비활성화', \
              '20': 'Anonymous FTP 비활성화', \
              '21': 'r 계열 서비스 비활성화', \
@@ -69,10 +71,24 @@ checkList = {'01': 'root 계정 원격 접속 제한', \
              '69': '로그온 시 경고 메시지 제공', \
              '70': 'NFS 설정파일 접근 제한', \
              '71': 'expn, vrfy 명령어 제한', \
-             '72': 'Apache 웹 서비스 정보 숨김', \
-             '42': '최신 보안패치 및 벤더 권고사항 적용', \
+             '72': 'Apache 웹 서비스 정보 숨김'}
+checkList_4 = {
+             '42': '최신 보안패치 및 벤더 권고사항 적용'}
+checkList_5 = {
              '43': '로그의 정기적 검토 및 보고', \
              '73': '정책에 따른 시스템 로깅 설정'}
+
+def logo():
+    print(" _     _                     ____                       _ _            ____ _               _    ")
+    print("| |   (_)_ __  _   ___  __  / ___|  ___  ___ _   _ _ __(_) |_ _   _   / ___| |__   ___  ___| | __")
+    print("| |   | | '_ \| | | \ \/ /  \___ \ / _ \/ __| | | | '__| | __| | | | | |   | '_ \ / _ \/ __| |/ /")
+    print("| |___| | | | | |_| |>  <    ___) |  __/ (__| |_| | |  | | |_| |_| | | |___| | | |  __/ (__|   < ")
+    print("|_____|_|_| |_|\__,_/_/\_\\  |____/ \___|\___|\__,_|_|  |_|\__|\__, |  \____|_| |_|\___|\___|_|\_\\")
+    print("                                                               |___/                              ")
+    print("이 프로그램은 KISA에서 발간한 주요통신기반시설 기술적 취약점 진단 및 평가 가이드를 토대로 제작되었습니다.")
+    print("이 프로그램은 차세대보안리더양성프로그램 BOB 9기 'BOB Linux 개발' 프로젝트 팀에 의해 제작되었습니다.")
+    print("(made by 김기서, 노무승, 이안나)")
+    print("")                          
 
 def checkAll():
     for i in range(1, 74):
@@ -81,33 +97,55 @@ def checkAll():
 
 def check():
     num = input('점검할 항목을 입력하세요: ')
+    if (num.isdecimal()):
+        num = int(num)
+        if (num >= 1) and (num <= 73):
+            command = 'python3 U_{0:02d}.py'.format(num)
+            os.system(command)
+        else:
+            print("1~73 사이 숫자만 입력해주세요.\n")
+            check()
+    else:
+        print("1~73 사이 숫자만 입력해주세요.\n")
+        check()
 
 def printList():
-    for key in checkList:
-        print('[U-{}] {}'.format(key, checkList[key]))
-        print('\n')
+    print("\t1. 계정 관리 관련")
+    for key in checkList_1:
+        print('\t\t[U-{}] {}'.format(key, checkList_1[key]))
+    print("\t2. 파일 및 디렉터리 관련")
+    for key in checkList_2:
+        print('\t\t[U-{}] {}'.format(key, checkList_2[key]))
+    print("\t3. 서비스 관리 관련")
+    for key in checkList_3:
+        print('\t\t[U-{}] {}'.format(key, checkList_3[key]))
+    print("\t4. 패치 관리 관련")
+    for key in checkList_4:
+        print('\t\t[U-{}] {}'.format(key, checkList_4[key]))
+    print("\t5. 로그 관리 관련")
+    for key in checkList_5:
+        print('\t\t[U-{}] {}'.format(key, checkList_5[key]))
+    print("")
 
 def main():
+    logo()
+
     while True:
         print('1. 모든 항목 점검')
         print('2. 특정 항목 점검')
         print('3. 점검 리스트 출력')
-        print('q. 종료\n')
+        print('4. 종료\n')
         select = input('실행할 명령을 선택하세요: ')
 
-        if select == 'q':
+        if select == '4':
             print('프로그램을 종료합니다.\n')
             break
-
         elif select == '1':
             checkAll()
-
         elif select == '2':
             check()
-
         elif select == '3':
             printList()
-
         else:
             print('잘못된 입력입니다. 다시 입력해주세요.\n')
 
