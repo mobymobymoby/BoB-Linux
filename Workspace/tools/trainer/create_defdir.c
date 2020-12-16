d377339bc1470762f050622fc55ed20eddd243b7
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2020-12-16 17:02:41 +0900

    Update create_defdir.c

--- Workspace/tools/trainer/create_defdir.c
+++ Workspace/tools/trainer/create_defdir.c
@@ -10,6 +10,19 @@ void create_defdir(void)
 	pwd = getpwuid(getuid());
 	snprintf(def_dir, sizeof(def_dir), "/home/%s/tr", pwd->pw_name);
 
+	/* 클라우드 트레이너 전용(여러 사용자가 이용 시 디렉토리 경로 설정) : 11번 라인 삭제 후 활성화
+	int i = 0;
+
+        for(i=0; i<100; i++)
+        {
+                snprintf(def_dir, sizeof(def_dir), "/home/%s/tr/%d", pwd->pw_name, i);
+                if(access(def_dir, 0) == 0)
+                        continue;
+                else
+                        break;
+        }
+	*/
+	
 	snprintf(rst_dir, sizeof(rst_dir), "rm -rf %s", def_dir);
 	system(rst_dir);
 	strncpy(rm_dir, rst_dir, sizeof(rm_dir));
